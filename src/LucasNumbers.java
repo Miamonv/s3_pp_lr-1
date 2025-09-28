845ccb8f162b835fcf25dbe45456cc51694d3a0c
Mariana <chaikovska.marianka@gmail.com>
2025-09-28 15:49:39 +0300

    final commit

---
+++ src/LucasNumbers.java
@@ -0,0 +1,40 @@
+import java.math.BigInteger;
+
+public class LucasNumbers {
+    private BigInteger sum = BigInteger.valueOf(3);
+
+    /**
+     * функція виконує всі обчислення чисел Люка, їх виведення для перевірки та виведення результату
+     * використання BigInteger!
+     * @param number скільки чисел Люка потрібно обраховувати
+     */
+    public void countLukasNumbers(int number){
+        BigInteger a = BigInteger.TWO;
+        BigInteger b = BigInteger.ONE;
+        BigInteger c;
+        BigInteger result = BigInteger.ZERO;
+        //check
+        System.out.println(a);
+        System.out.println(b);
+        for (int i = 2; i < number; i++){
+            c = a.add(b);       // a + b для BigInteger
+            //check
+            System.out.println(c);
+            result = this.countSum(c);
+            a = b;
+            b = c;
+        }
+        System.out.println("Сума чисел Люка на заданому діапазоні: " + result);
+    }
+
+    /**
+     * Додає задане число Люка до поточної накопиченої суми.
+     *
+     * @param c поточне число, яке додається до суми
+     * @return сума, яка постійно оновлюється при кожній новій ітерації
+     */
+    public BigInteger countSum (BigInteger c) {
+        sum = sum.add(c);
+        return sum;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+class Input{
+    /**
+     * отримує необхідну кількість чисел Люка від користувача або з командного рядка
+     *
+     * @param args аргументи командного рядка, які можуть містити число number
+     * @return скільки чисел Люка потрібно генерувати
+     */
+    public int getN(String[] args){
+        int number;
+        if (args.length > 0){
+            number = Integer.parseInt(args[0]);
+        }
+        else {
+            System.out.println("Введіть кількість чисел Люка: ");
+            Scanner input = new Scanner(System.in);    //для читання з клавіатури
+
+            number =  input.nextInt();
+            input.close();
+        }
+        return number;
+    }
+}
+
+public class Main {
+    /**
+     * основна логіка запуску інших методів
+     */
+    void main(String[] args) {
+        int number;
+
+        Input enter = new Input();      //введення
+        number = enter.getN(args);
+
+        LucasNumbers calculator = new LucasNumbers();   //запуск обчислень
+        calculator.countLukasNumbers(number);
+    }
+}
